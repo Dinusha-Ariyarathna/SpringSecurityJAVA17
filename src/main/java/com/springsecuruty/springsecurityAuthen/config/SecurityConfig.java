a1b726febb4332477a48b43d3660a498114b9a4a
dinusha_d <dinushaariyarathna2@gmail.com>
2025-01-03 04:37:55 +0530

    2 CONFIGURATION METHODS IMPLEMETENTED AND CUSTOM UI ADDED.

--- src/main/java/com/springsecuruty/springsecurityAuthen/config/SecurityConfig.java
+++ src/main/java/com/springsecuruty/springsecurityAuthen/config/SecurityConfig.java
@@ -21,5 +21,53 @@ import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
+    @Order(1)
+//    A Spring Security filter chain for the Protocol Endpoints.
+    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http)
+            throws Exception {
+        OAuth2AuthorizationServerConfigurer authorizationServerConfigurer =
+                OAuth2AuthorizationServerConfigurer.authorizationServer();
 
+        http
+                .securityMatcher(authorizationServerConfigurer.getEndpointsMatcher())
+                .with(authorizationServerConfigurer, (authorizationServer) ->
+                        authorizationServer
+                                .oidc(Customizer.withDefaults())	// Enable OpenID Connect 1.0
+                )
+                .authorizeHttpRequests((authorize) ->
+                        authorize
+                                .anyRequest().authenticated()
+                )
+                // Redirect to the login page when not authenticated from the
+                // authorization endpoint
+                .exceptionHandling((exceptions) -> exceptions
+                        .defaultAuthenticationEntryPointFor(
+                                new LoginUrlAuthenticationEntryPoint("/login"),
+                                new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+                        )
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
+//    A Spring Security filter chain for authentication.
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
+            throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/login", "/css/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                // Form login handles the redirect to the login page from the
+                // authorization server filter chain
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll()
+                );
+
+        return http.build();
+    }
 }
---
+++ src/main/java/com/springsecuruty/springsecurityAuthen/controller/LoginController.java
@@ -0,0 +1,21 @@
+package com.springsecuruty.springsecurityAuthen.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @AUTHOR : Dinusha Ariyarathna
+ * @DATE : 1/3/2025
+ * @PROJECT : springsecurityAuthen
+ */
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+}
